d3d81990c11a1eee8beea8bf0a6a3325420fd733
Tim Heilman <tim@dv8.org>
2016-10-06 09:44:17 -0700

    Rest of files for leiningen project added by IDEA.

---
+++ test/gof_in_clojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns gof-in-clojure.core-test
+  (:require [clojure.test :refer :all]
+            [gof-in-clojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
