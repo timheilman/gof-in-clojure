299711705524c0e231439749a7028f99b92cfe5c
Tim Heilman <tim@dv8.org>
2016-10-10 13:47:32 -0700

    Get lein run to work.

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject gof-in-clojure "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :description (str "make a set of true/false flashcards to memorize the many-to-many relationship "
+                    "between patterns and common causes of redesign from section 1.6 of GoF Design Patterns")
+  :url "http://github.com/timheilman"
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :main gof-in-clojure.core)
--- src/gof_in_clojure/DesignPatternsGofFlashcardGeneration.clj
+++ src/gof_in_clojure/DesignPatternsGofFlashcardGeneration.clj
@@ -1,7 +1,3 @@
-;; motivation:
-;; make a set of true/false flashcards to memorize the many-to-many relationship 
-;; between patterns and common causes of redesign from section 1.6 of GoF Design Patterns
-
 (declare cartesian-product)
 (defn cartesian-product-on-nonempty-list [multiplicands]
       (for [firstElement (first multiplicands)
