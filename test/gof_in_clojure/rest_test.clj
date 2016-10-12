f01a5607fa2b7faede66c1b1b3ea291d83b465b0
Tim Heilman <tim@dv8.org>
2016-10-12 12:50:11 -0700

    Tests for the cartesian product.

--- test/gof_in_clojure/rest_test.clj
+++ test/gof_in_clojure/rest_test.clj
@@ -2,6 +2,21 @@
   (:require [clojure.test :refer :all]
             [gof-in-clojure.rest :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-base-case
+  (testing "The cartesian product of 0 lists ought to be the empty list."
+    (is (= '(())  (cartesian-product '())))))
+
+(deftest test-single-vector
+  (testing "The cartesian product of 1 lists ought to be itself decomposed."
+    (is (= '((0) (1) (2) (3) (4))  (cartesian-product `(~(range 5)))))))
+
+(deftest test-two-vectors
+  (testing "The cartesian product of 2 lists ought to be a grid."
+    (is (= '((0 0) (0 1) (0 2) (1 0) (1 1) (1 2))  (cartesian-product `(~(range 2) ~(range 3)))))))
+
+(deftest test-three-vectors
+  (testing "The cartesian product of 3 lists ought to be a parallelopiped.")
+  (is (= '((0 0 0) (0 0 1) (0 0 2) (0 1 0) (0 1 1) (0 1 2) (1 0 0) (1 0 1) (1 0 2) (1 1 0) (1 1 1) (1 1 2))
+         (cartesian-product `(~(range 2) ~(range 2) ~(range 3))))))
+
+
